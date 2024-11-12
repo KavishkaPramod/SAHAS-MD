202e664186c93cbedbf5ac9b955f0d3f757327ea
KavishkaPramod <pramodkavishka133@gmail.com>
2024-11-12 15:20:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "pPFBDY5R#QnYz8pi_9ZNwDwoMUV-NmpiiBS8Zw25NulyJ8LPWoDw",
+  SESSION_ID: process.env.SESSION_ID || "ZDszmZgB#yGSmGfOnJNPLrogjP2YXoOLXVz382NMqJ4jvNnOKzFM",
   // ADD Your Session Id 
   MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
     // ADD Your MongoDB Database URL
